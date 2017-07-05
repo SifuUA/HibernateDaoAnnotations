ad32667f0c22316ec3e6143ef7697920fe366661
SifuUA <alekseykres@gmail.com>
2017-07-05 19:25:32 +0300

    Initial commit

---
+++ src/main/java/businessLogig/HibernateUtil.java
@@ -0,0 +1,32 @@
+package businessLogig;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by Alex on 05.07.2017.
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            return configuration.buildSessionFactory();
+        }catch (Throwable e){
+            System.out.println("Initial Sessionfactory creation faiked." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public static void shutdown(){
+        getSessionFactory().close();
+    }
+
+
+}
