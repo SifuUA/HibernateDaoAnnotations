e16bdf9ae33da290a0b928983094536d553079b4
SifuUA <alekseykres@gmail.com>
2017-07-05 21:14:16 +0300

    add AddressService

--- src/main/java/service/AddressService.java
+++ src/main/java/service/AddressService.java
@@ -3,6 +3,8 @@ package service;
 import businessLogig.SessionUtil;
 import dao.AddressDao;
 import entity.Address;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -12,22 +14,61 @@ import java.util.List;
  */
 public class AddressService extends SessionUtil implements AddressDao {
     public void add(Address address) throws SQLException {
+
+        //open Session
         openTransactionSession();
+
+        Session session = getSession();
+        session.save(address);
+
+        //close Session
+        closeTransactionSession();
     }
 
     public List<Address> getAll() throws SQLException {
-        return null;
+        openTransactionSession();
+
+        String sql = "SELECT * FROM ADDRESS";
+
+        Session session = getSession();
+        Query query = session.createNativeQuery(sql).addEntity(Address.class);
+        List<Address> addresses = query.list();
+
+        closeTransactionSession();
+
+        return addresses;
     }
 
     public Address getById(Long id) throws SQLException {
-        return null;
+
+        openTransactionSession();
+
+        String sql = "SELECT FROM ADDRESS WHERE ID = :id";
+        Session session = getSession();
+        Query query = session.createNativeQuery(sql).addEntity(Address.class);
+        query.setParameter("id", id);
+
+        Address address = (Address)query.getSingleResult();
+
+        return address;
     }
 
     public void update(Address address) throws SQLException {
+        openTransactionSession();
 
+        Session session = getSession();
+        session.update(address);
+
+        closeTransactionSession();
     }
 
     public void remove(Address address) throws SQLException {
 
+        openTransactionSession();
+
+        Session session = getSession();
+        session.remove(address);
+
+        closeTransactionSession();
     }
 }
